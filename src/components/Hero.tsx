68729eaa18657c6d4e021d7285935588036066fb
doertyhansen <doerty.hansen@gmail.com>
2026-01-09 14:12:17 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,11 +28,11 @@ const Hero = () => {
         </p>
         
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+        {/* <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-foreground/30 rounded-full flex justify-center pt-2">
             <div className="w-1 h-2 bg-foreground/50 rounded-full" />
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
