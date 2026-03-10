609482707408ece28d75fa7cf7d3f02f01ae7d92
doertyhansen <doerty.hansen@gmail.com>
2026-03-10 10:16:07 +0100

    Replace commented tour date with upcoming date

--- src/components/TourDates.tsx
+++ src/components/TourDates.tsx
@@ -1,7 +1,7 @@
 const tourDates = [
   { date: "12. DEZ 2025", city: "ERLANGEN", venue: "Rock Up", status: "sold out" },
   { date: "18. DEZ 2025", city: "NÜRNBERG", venue: "MUZ Club", status: "sold out" },
-  /*{ date: "07. FEB 2026", city: "ERLANGEN", venue: "E-Werk", status: "upcoming" }*/
+  { date: "03. MAI 2026", city: "BURGHAUSEN", venue: "MUSIC FOR PEACE", status: "upcoming" }
 ];
 
 const TourDates = () => {
